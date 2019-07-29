3e1c5785bee438ae07831102e23c03806883b5dc
Eduardo <sutil.edu@gmail.com>
2019-07-29 09:25:47 -0300

    Commit

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,2 +1,20 @@
 /* SystemJS module definition */
+
+// interface NodeRequireFunction {
+//     /* tslint:disable-next-line:callable-types */
+//     (id: string): any;
+// }
+
+// interface NodeModule {
+//     exports: any;
+//     require: NodeRequireFunction;
+//     id: string;
+//     filename: string;
+//     loaded: boolean;
+//     parent: NodeModule | null;
+//     children: NodeModule[];
+//     paths: string[];
+// }
+
+/// <reference path="../node_modules/@types/node/index.d.ts" />
 declare var module: NodeModule;
